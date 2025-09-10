53ac3080807e91cd2aa34553dc01689d82577f1c
Олександр Шевчук <bluznyku@gmail.com>
2025-09-10 09:27:40 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,31 @@
+age = 16
+weight = 62.5
+dad_weight = 90.3
+name = "Олександр"
+dad_name = "Юрій"
+favourite_days = ["Пятниця" , "Субота" , "Неділя"]
+im_human = True
+where_i_live = {"country ":"Україна" ,"city": "Луцьк"}
+mnoz = { 1, 2 , 3}
+birthday = (2 , 3, 2009)
+
+print("type", type(age))
+print("type", type (weight))
+print ( "type", type (dad_weight))
+print ("type", type(name))
+print ("type", type (dad_name ))
+print ("type", type(favourite_days))
+print ("type", type(im_human))
+print("type", type(where_i_live))
+print ("type", type(mnoz))
+print("type", type(birthday))
+print (age)
+print (weight)
+print (dad_weight)
+print (name)
+print (dad_name )
+print (favourite_days)
+print (im_human)
+print (where_i_live)
+print (mnoz)
+print (birthday)
