46415c73d689b5c6fabacc710561ffccac096544
Олександр Шевчук <bluznyku@gmail.com>
2025-10-12 18:13:25 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -49,8 +49,9 @@ while True:
             print(f"Хорошисти (7-9): {len(good)} ( {', '.join(good)})")
             print(f"Відстаючі (4-6): {len( struggling)} ( {', '.join( struggling) })")
             print(f"Не здали (1-3): {len(failed)} ({', '.join(failed) })")
-    elif vubir() == "stop":
+    elif vubir == "stop":
         print("Дякую що відвідали нашу програму")
         break
     else:
         print("Ви ввели неправильну дію. Спробуйте ще раз")
+
