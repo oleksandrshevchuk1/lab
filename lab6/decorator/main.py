4b1e48825702bf425873e8af98a081c0a2fa587f
Олександр Шевчук <bluznyku@gmail.com>
2025-11-03 22:57:22 +0200

    Add files via upload

---
+++ lab6/decorator/main.py
@@ -0,0 +1,15 @@
+from cache_decorator import cache_with_ttl
+import time
+
+# Використовуємо наш декоратор із TTL 5 секунд
+@cache_with_ttl(ttl_seconds=5)
+def add_numbers(a, b):
+    print(f"Обчислюємо {a} + {b}")
+    return a + b
+
+# Виклики функції
+print(add_numbers(2, 3))  # Перше обчислення
+time.sleep(2)
+print(add_numbers(2, 3))  # Використає кеш
+time.sleep(5)
+print(add_numbers(2, 3))  # Кеш протермінувався, обчислюємо заново
