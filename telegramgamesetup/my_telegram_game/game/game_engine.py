119fce4485202af00b2814d5f91a24074ce3f5ba
Шевчук <bluznyku@gmail.com>
2025-11-18 11:58:33 +0200

    Update game_engine.py

--- telegramgamesetup/my_telegram_game/game/game_engine.py
+++ telegramgamesetup/my_telegram_game/game/game_engine.py
@@ -2,6 +2,3 @@ user_progress = {}
 
 def set_user_progress(chat_id, node_id):
     user_progress[chat_id] = node_id
-
-def get_user_progress(chat_id):
-    return user_progress.get(chat_id, "start")
